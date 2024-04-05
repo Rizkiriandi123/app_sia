6b120f5053c8f54b52fbf5f1bbaca8d8437fceb1
Rizkiriandi123 <rizkiriandi594@gmail.com>
2024-04-05 19:56:24 +0700

    update

--- sidebar.php
+++ sidebar.php
@@ -1,8 +1,8 @@
 <?php
 if(!isset($_GET['modul'])){
-    $modul = "";
+$modul = "";
 }else{
-    $modul = $_GET['modul'];
+$modul = $_GET['modul'];
 }
 ?>
 <ul class="nav nav-pills flex-column ms-2">
@@ -11,53 +11,57 @@ if(!isset($_GET['modul'])){
 <i class="bi bi-speedometer2 me-2"></i>Dashboard
 </a>
 </li>
-<li class="nav-item">
-<a href="?modul=akun" class="nav-link text-white <?= $modul 
-=='akun'?'active':''; ?>">
+ <li class="nav-item">
+<a href="?modul=akun" class="nav-link text-white <?= $modul
+== 'akun'?'active':''; ?>">
 <i class="bi bi-database-fill-gear me-2"></i></i>Data Akun
 </a>
 </li>
 <li class="nav-item">
-<a href="#transaksiCollapse" data-bs-toggle="collapse" aria-expanded="false" class="nav-link d-flex text-white">
+<a href="#transaksiCollapse" data-bs-toggle="collapse" aria-
+expanded="false" class="nav-link d-flex text-white">
 <i class="bi bi-cash-stack me-2"></i>
 Transaksi
 <i class="bi bi-caret-down-fill ms-auto"></i>
 </a>
-<ul class="nav nav-pills flex-column collapse ms-4 bg-white rounded p-2"id="transaksiCollapse">
+<ul class="nav nav-pills flex-column collapse ms-4 bg-white rounded p-2" id="transaksiCollapse">
 <li class="nav-item">
-<a href="?modul=pembelian" class="nav-link text-dark text-decoration-none <?= $modul=='pembelian'?'active':''; ?>">Pembelian</a>
+<a href="?modul=pembelian" class="nav-link text-dark text-
+decoration-none <?= $modul=='pembelian'?'active':''; ?>">Pembelian</a>
 </li>
 <li class="nav-item">
-<a href="?modul=pembayaran" class="nav-link text-dark text-decoration-none <?= $modul=='pembayaran'?'active':''; ?>">Pembayaran</a>
+<a href="?modul=pembayaran" class="nav-link text-dark text- decoration-none <?= $modul=='pembayaran'?'active':''; ?>">Pembayaran</a>
 </li>
 <li class="nav-item">
-<a href="?modul=penjualan" class="nav-link text-dark text-decoration-none <?= $modul=='penjualan'?'active':''; ?>">Penjualan</a>
+<a href="?modul=penjualan" class="nav-link text-dark text- decoration-none <?= $modul=='penjualan'?'active':''; ?>">Penjualan</a>
 </li>
 </ul>
 </li>
 <li class="nav-item">
-<a href="#dataCollapse" data-bs-toggle="collapse" aria-expanded="false"class="nav-link d-flex text-white">
+<a href="#dataCollapse" data-bs-toggle="collapse" aria-expanded="false" class="nav-link d-flex text-white">
 <i class="bi bi-database-fill-gear me-2"></i>
 Master Data
 <i class="bi bi-caret-down-fill ms-auto"></i>
 </a>
-<ul class="nav nav-pills flex-column collapse ms-4 bg-white rounded p-2"id="dataCollapse">
+<ul class="nav nav-pills flex-column collapse ms-4 bg-white rounded p-2" id="dataCollapse">
 <li class="nav-item">
-<a href="?modul=pelanggan" class="nav-link text-dark text-decoration-none <?= $modul=='pelanggan'?'active':''; ?>">Pelanggan</a>
+<a href="?modul=pelanggan" class="nav-link text-dark text- decoration-none <?= $modul=='pelanggan'?'active':''; ?>">Pelanggan</a>
 </li>
 <li class="nav-item">
-<a href="?modul=barang" class="nav-link text-dark text-decoration-none <?= $modul=='barang'?'active':''; ?>">Barang</a>
+<a href="?modul barang" class="nav-link text-dark text-decoration-
+none <?= $modul=='barang'?'active':''; ?>">Barang</a>
 </li>
 <li class="nav-item">
-<a href="?modul=suplier" class="nav-link text-dark text-decoration-none <?= $modul=='suplier'?'active':''; ?>">Suplier</a>
+<a href="?modul=suplier" class="nav-link text-dark text-decoration-
+none <?= $modul=='suplier'?'active':''; ?>">Suplier</a>
 </li>
 <li class="nav-item">
-<a href="?modul=pengguna" class="nav-link text-dark text-decoration-none <?= $modul=='pengguna'?'active':''; ?>">Pengguna</a>
+<a href="?modul=pengguna" class="nav-link text-dark text-decoration- none <?= $modul== 'pengguna'?'active':''; ?>">Pengguna</a>
 </li>
 </ul>
 </li>
 <li class="nav-item">
-<a href="?modul=jurnal" class="nav-link text-white <?=$modul=='jurnal'?'active':''; ?>">
+<a href="?modul=jurnal" class="nav-link text-white <?= $modul=='jurnal'?'active':''; ?>">
 <i class="bi bi-clipboard2-data-fill me-2"></i>Jurnal Umum
 </a>
 </li>
