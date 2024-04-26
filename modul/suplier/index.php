30fc5b160fd11d31597f08d0ccb59e8f96cde42b
Rizkiriandi123 <163546305+Rizkiriandi123@users.noreply.github.com>
2024-04-26 21:00:55 +0700

    Update index.php

--- modul/suplier/index.php
+++ modul/suplier/index.php
@@ -1,11 +1,36 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>document</title>
-</head>
-<body>
-    <h2>suplier</h2>
-</body>
-</html>
+<?php
+if(!isset($_GET['modul'])){
+include_once('modul/home/index.php');
+}else{
+if($_GET['modul']=="transaksi"){
+include_once('modul/transaksi/index.php');
+}elseif($_GET['modul']=="akun"){
+include_once('modul/akun/index.php');
+}elseif($_GET['modul']=="pembelian"){
+include_once('modul/pembelian/index.php');
+}elseif($_GET['modul']=="pembayaran") {
+include_once('modul/pembayaran/index.php');
+}elseif($_GET['modul']=="penjualan"){
+include_once('modul/penjualan/index.php');
+}elseif($_GET['modul']=="pelanggan"){
+include_once('modul/pelanggan/index.php');
+}elseif($_GET['modul']=="barang"){
+include_once('modul/barang/index.php');
+}elseif($_GET['modul']=="suplier"){
+include_once('modul/suplier/index.php');
+}elseif($_GET['modul']=="pengguna"){
+include_once('modul/pengguna/index.php');
+}elseif($_GET['modul']=="jurnal"){
+include_once('modul/jurnal-umum/index.php');
+}elseif($_GET['modul']=="profile"){
+include_once('modul/profile/index.php');
+}else{
+?>
+<div class="alert alert-info">
+<strong>Perhatian: </strong>
+Modul yang anda request tidak ada!!!
+</div>
+<?php
+}
+}
+?>
