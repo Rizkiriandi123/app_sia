062df1f228489ced96de0159027b02a8ee4b6eb5
Rizkiriandi123 <rizkiriandi594@gmail.com>
2024-06-14 18:56:28 +0700

    update

--- modul/profile/index.php
+++ modul/profile/index.php
@@ -1,46 +1,50 @@
-<?php
-if(isset($_POST['submit'])){
-session_start();
-include_once('../../koneksi.php');
-$username = $_POST['username'];
-$nama_lengkap = $_POST['nama_lengkap'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$password_baru = $_POST['password_baru'];
-$password_ulang = $_POST['password_ulang'];
-if(empty($password)) {
-$query = "UPDATE tbl_pengguna SET nama_lengkap='$nama_lengkap',
-email='$email' WHERE username='$username'";
-$exec = mysqli_query($koneksi, $query);
-if($exec){
-$_SESSION['pesan'] = "Data profile telah diperbaharui";
-header('location:../../dashboard.php?modul=profile');
-}else{
-$_SESSION['pesan'] = "Data profile gagal diperbaharui"; header('location:../../dashboard.php?modul=profile');
-}
-}else{
-$query = "SELECT * FROM tbl_pengguna WHERE username='$username'";
-$exec = mysqli_query($koneksi, $query);
-$data = mysqli_fetch_array($exec);
-if(password_verify($password, $data['password'])){
-if($password_baru == $password_ulang) {
-$password = password_hash ($password_baru, PASSWORD_BCRYPT);
-$query = "UPDATE tbl_pengguna SET password='$password', nama_lengkap='$nama_lengkap', email='$email' WHERE username='$username'";
-$exec = mysqli_query($koneksi, $query);
-if($exec) {
-$_SESSION['pesan'] = "Data profile telah diperbaharui";
-header('location:../../dashboard.php?modul=profile');
-}
-}else{
-$_SESSION['pesan'] = "Password baru tidak sesuai";
-header('location:../../dashboard.php?modul=profile');
-}
-}else{
-$_SESSION['pesan'] = "Password lama tidak sesuai";
-header('location:../../dashboard.php?modul=profile');
-}
-}
-}else{
-header('location:../../index.php');
-}
-?>
+
+ <div class="card mb-3">
+<div class="card-body">
+ <form action="modul/profile/aksi_profile.php" method="post">
+<div class="row">
+ <div class="mb-3 col-md-4">
+ <label for="username" class="form-label">Username</label>
+<input type="text" class="form-control" name="username" value="<?=
+$data['username'] ?>" readonly>
+ </div>
+<div class="mb-3 col-md-4">
+     <label for="nama_lengkap" class="form-label">Nama lengkap</label>
+<input type="text" class="form-control" name="nama_lengkap"
+value="<?= $data['nama_lengkap'] ?>">
+ </div>
+<div class="mb-3 col-md-4">
+<label for="email" class="form-label">Email</label>
+<input type="email" class="form-control" name="email" value="<?=
+$data['email'] ?>">
+ </div>
+ </div>
+ <div class="row">
+<div class="mb-3 col-md-4">
+<label for="password" class="form-label">Password</label>
+ <input type="password" class="form-control" name="password">
+ </div>
+ <div class="mb-3 col-md-4">
+ <label for="password_baru" class="form-label">Password baru</label>
+<input type="password" class="form-control" name="password_baru">
+</div>
+<div class="mb-3 col-md-4">
+<label for="password_ulang" class="form-label">Ulangi password</label>
+<input type="password" class="form-control" name="password_ulang">
+ </div>
+ </div>
+ <hr class="text-secondary">
+<div class="d-flex">
+     <span class="me-auto text-gray">
+ <?php
+ if(isset($_SESSION['pesan'])){
+ echo $_SESSION['pesan'];
+ unset($_SESSION['pesan']);
+ }
+ ?>
+</span>
+<button type="submit" name="submit" class="btn btnprimary">Perbaharui</button>
+ </div>
+</form>
+</div>
+</div>
